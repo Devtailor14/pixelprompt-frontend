404f086c819c8ec4c407f046c84e90fc30e95cf2
devtailor <devtailor81@gmail.com>
2025-06-07 18:45:27 +0530

    fixed: import path home

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react'
 import {Routes, Route} from 'react-router-dom'
 import { ToastContainer, } from 'react-toastify';
   import 'react-toastify/dist/ReactToastify.css';
-import Home from './pages/home'
+import Home from './pages/Home';
 import Buycredit from './pages/BuyCedit'
 import Result from './pages/result'
 import Navbar from './components/Navbar'
