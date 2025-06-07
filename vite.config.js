fa8b9ebc54d3b7744edea4046eb008f3cbb85056
devtailor <devtailor81@gmail.com>
2025-06-07 18:58:01 +0530

    vite preview

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   resolve: {
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  preview: {
+    allowedHosts: ['pixelprompt-frontend.onrender.com'],
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173
+  }
 })
